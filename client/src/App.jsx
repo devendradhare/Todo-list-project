28a6985f891f69554c8deed41de6cff84d358bc0
devendra dhare <devendradhare03@gmail.com>
2023-11-27 10:20:48 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="main">
+        <h1 className="head">ToDo-List</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
